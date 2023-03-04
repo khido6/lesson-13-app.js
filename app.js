4eafd4a1b2b6255a024bf3f6c828255158954f51
khido6 <iraklixidesheli@gmail.com>
2023-03-04 16:28:06 +0400

    app.js in html

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ console.log("typeof userSurnamer", typeof userSurname);
 let userAge = '26';
 console.log("typeof userAge", typeof userAge);
 
+const myboolean = true;
+console.log("typeof myboolean", typeof myboolean);
+
+let myUndefined;
+console.log("typeof myUndefined", typeof myUndefined);
+
 const myHobby = 'coding';
 console.log("typeof myHobby", typeof myHobby);
 
 const yearOfBirth = '1996';
 console.log("typeof myHobby", typeof myHobby);
 
+
 const currentYear = '2023';
 console.log("typeof myHobby", typeof myHobby);
 
-const myText = 'my name is ${irakli} + myHobby ${coding} + mybirthdate ${1996};
+const myText = `my name is ${irakli} + myHobby ${coding} + mybirthdate ${1996}`;
 console.log(myText);
