a1f08226ba0327e8ee243d2776c3bdb8ec2d7f9a
khido6 <iraklixidesheli@gmail.com>
2023-03-04 16:10:10 +0400

    added js

---
+++ app.js
@@ -0,0 +1,20 @@
+let userName = 'irakli';
+console.log("typeof username", typeof username);
+
+let userSurname = 'khido';
+console.log("typeof userSurnamer", typeof userSurname);
+
+let userAge = '26';
+console.log("typeof userAge", typeof userAge);
+
+const myHobby = 'coding';
+console.log("typeof myHobby", typeof myHobby);
+
+const yearOfBirth = '1996';
+console.log("typeof myHobby", typeof myHobby);
+
+const currentYear = '2023';
+console.log("typeof myHobby", typeof myHobby);
+
+const myText = 'my name is ${irakli} + myHobby ${coding} + mybirthdate ${1996};
+console.log(myText);
