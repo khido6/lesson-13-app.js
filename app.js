bcfbf0d943e59b45f5038d26ea98a70aa6658424
khido6 <iraklixidesheli@gmail.com>
2023-03-06 19:01:50 +0400

    bolo

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 let userName = 'irakli';
-console.log("typeof username", typeof username);
-
 let userSurname = 'khido';
-console.log("typeof userSurnamer", typeof userSurname);
-
 let userAge = '26';
-console.log("typeof userAge", typeof userAge);
 
 const myboolean = true;
-console.log("typeof myboolean", typeof myboolean);
+const mynull = null;
 
-let myUndefined;
-console.log("typeof myUndefined", typeof myUndefined);
+const myhobby = "coding",
+      yearOfBirth = "1996",  
+      currentYear = "2023";
+      
 
-const myHobby = 'coding';
 console.log("typeof myHobby", typeof myHobby);
 
-const yearOfBirth = '1996';
-console.log("typeof myHobby", typeof myHobby);
 
+const result = `username is ${irakli} + myHobby ${coding} + yearofbirth ${1996}`;
+console.log(result);
 
-const currentYear = '2023';
-console.log("typeof myHobby", typeof myHobby);
-
-const myText = `my name is ${irakli} + myHobby ${coding} + mybirthdate ${1996}`;
-console.log(myText);
+ 
